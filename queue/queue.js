f8660181c3fa4a58242e5349d1179e22dda8be3b
Abhijeet Mishra <abhijeet.mishra@yulu.bike>
2023-07-09 14:57:23 +0530

    Added queue changes

--- queue/queue.js
+++ queue/queue.js
@@ -2,23 +2,14 @@
 class Queue {
     constructor() {
         this.data = [];
-        this.front = 0;
-        this.back = 0;
     }
 
     push(data) {
-        this.data[this.back] = data;
-        ++this.back;
+        this.data.push(data);
     }
 
     pop() {
-        let pop_out_data = this.data[this.front];
-        this.data[this.front] = null;
-        ++this.front;
-        if (this.front > this.back) {
-            this.front = 0;
-            this.back = 0; 
-        }
+        let pop_out_data = this.data.shift();
         return pop_out_data;
     }
 }
--- tree/level_order_traversal.js
+++ tree/level_order_traversal.js
@@ -1,5 +1,8 @@
 let {createQueue} = require('../queue/Queue')
 
+let { Tree } = require('./Tree')
+let root = Tree();
+
 function print_level_order(node) {
     let queue = createQueue();
     queue.push(node);
@@ -17,33 +20,6 @@ function print_level_order(node) {
    
 }
 
-function create_tree() {
-
-    class Node {
-        constructor(data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
-    
-    let root = new Node(10);
-    root.left = new Node(5);
-    root.right = new Node(15);
-    
-    root.left.left = new Node(25);
-    root.left.right = new Node(35);
-    
-    root.right.left = new Node(45);
-    root.right.right = new Node(55);
-    
-    root.left.left.left = new Node(100);
-    
-    return root
-}
-
-
-let root = create_tree();
 print_level_order(root);
 
 
