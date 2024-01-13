3323874ae90fcad5d42383038ea5de515a02a223
Abhijeet Mishra <abhijeet.mishra@yulu.bike>
2024-01-14 00:57:39 +0530

    added sub sequesnce

--- recursion/possible_sequence.js
+++ recursion/possible_sequence.js
@@ -1,4 +1,5 @@
 
+// strings
 function possible_sequence(str, i, current) {
     
     if (i == str.length) {
@@ -11,3 +12,20 @@ function possible_sequence(str, i, current) {
 }
 
 possible_sequence("abc", 0, "");
+
+function array_sequence(arr, i, ds ,size) {
+    
+    if (i >= size) {
+        console.log(ds)
+        return;
+    };
+
+    ds.push(arr[i]);
+    array_sequence(arr, i + 1, ds, size); // include
+    ds.pop(); // since call by reference so we have to pop
+    
+    array_sequence(arr, i + 1, ds, size); // dont include
+
+}
+
+ array_sequence([3,1,2] , 0 , [] , 3)
